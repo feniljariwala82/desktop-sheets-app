58b7c0dd7b2cb356b58f28f868a414a665240909
fenil jariwala <feniljariwala82@gmail.com>
2023-10-24 19:33:03 +0530

    assets, height and width updated

--- renderer/src/pages/Home.tsx
+++ renderer/src/pages/Home.tsx
@@ -3,9 +3,10 @@ import { SpreadsheetComponent } from "@syncfusion/ej2-react-spreadsheet";
 const Home = () => {
   return (
     <SpreadsheetComponent
-      allowResizing // allowing auto resizing
       height={window.innerHeight} // setting component height to the inner height of the window
       width={window.innerWidth} // setting component width to the inner width of the window
+      allowResizing
+      style={{ width: "100%", maxWidth: "100%" }}
     />
   );
 };
